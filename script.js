b34181388209305e19d7e18204e2de4baa7e1f0e
priyadharsini02 <108711214+priyadharsini02@users.noreply.github.com>
2024-05-19 12:06:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.read-it-button1').addEventListener('click', function() {
+      window.location.href = 'https://www.berkshirehathaway.com/SpecialLetters/WEB%20past%20present%20future%202014.pdf';
+    });
+  
+    document.querySelector('.read-it-button2').addEventListener('click', function() {
+      window.location.href = 'https://www.berkshirehathaway.com/SpecialLetters/CTM%20past%20present%20future%202014.pdf';
+    });
+  });
+  
