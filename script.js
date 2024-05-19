5de67ad9878a6aec608441e76d4343703abcad72
priyadharsini02 <108711214+priyadharsini02@users.noreply.github.com>
2024-05-19 17:11:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.read-it-button1').addEventListener('click', function() {
-      window.location.href = 'https://www.berkshirehathaway.com/SpecialLetters/WEB%20past%20present%20future%202014.pdf';
-    });
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.querySelector('.read-it-button1').addEventListener('click', function() {
+//       window.location.href = 'https://www.berkshirehathaway.com/SpecialLetters/WEB%20past%20present%20future%202014.pdf';
+//     });
   
-    document.querySelector('.read-it-button2').addEventListener('click', function() {
-      window.location.href = 'https://www.berkshirehathaway.com/SpecialLetters/CTM%20past%20present%20future%202014.pdf';
-    });
-  });
+//     document.querySelector('.read-it-button2').addEventListener('click', function() {
+//       window.location.href = 'https://www.berkshirehathaway.com/SpecialLetters/CTM%20past%20present%20future%202014.pdf';
+//     });
+//   });
   
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const modal = document.getElementById("modal");
+  const btn = document.getElementById("openModal");
+  const span = document.getElementsByClassName("close-button")[0];
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+    document.body.classList.add('modal-open');
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none";
+    document.body.classList.remove('modal-open');
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      document.body.classList.remove('modal-open');
+    }
+  }
+});
